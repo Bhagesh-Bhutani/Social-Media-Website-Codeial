7bc4cbeb207842f4d132668016061fc6e6702547
Bhagesh-Bhutani <bhutanibhagesh49@gmail.com>
2021-07-30 01:34:29 +0530

    Added another layer of Authentication: Nobody makes posts without logging in

--- routes/posts.js
+++ routes/posts.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 const posts_controller = require('../controllers/posts_controller');
 
-router.post('/', posts_controller.create);
+router.post('/', passport.sign_out_handler, posts_controller.create);
 
 module.exports = router;

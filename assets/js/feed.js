ab2041f73099afd30b372b1c1c10eefa1c119b87
Bhagesh-Bhutani <bhutanibhagesh49@gmail.com>
2021-07-30 18:12:49 +0530

    Added scroll to top button

--- assets/js/feed.js
+++ assets/js/feed.js
@@ -1,7 +1,18 @@
-let like_comment_tab = document.getElementById('like-comment-tab');
+let scroll_to_top_btn = document.getElementById('scroll-to-top-btn');
 
-like_comment_tab.addEventListener('click', function(event){
-    if(event.target.classList.contains('comment-btn')){
-
+window.addEventListener('scroll', function(event){
+    if(document.documentElement.scrollTop > 50){
+        scroll_to_top_btn.classList.add('fade-opacity');
+        scroll_to_top_btn.classList.remove('transparent-opacity');
+    } else {
+        scroll_to_top_btn.classList.remove('fade-opacity');
+        scroll_to_top_btn.classList.add('transparent-opacity');
     }
+});
+
+scroll_to_top_btn.addEventListener('click', function(event){
+    document.documentElement.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
 });
